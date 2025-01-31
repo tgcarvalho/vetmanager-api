4cd14418792a491d18cf3f17ceb573cb2daa9175
Thiago Carvalho <thiago.xsource@gmail.com>
2025-01-31 11:37:38 -0300

    chore(infra): initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/teste', (req, res)=> {
+    res.status(200)
+    .send({message: 'boas-vindas à API'})
+})
+
+module.exports = app;
